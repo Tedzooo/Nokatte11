aa0d581e09edad9a4d189fba18131e2896b36660
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-26 21:02:49 +0530

    Create test.py

---
+++ pyrogrambot/tedzo/test.py
@@ -0,0 +1,64 @@
+from pyrogram.enums import ParseMode
+from pyrogram import Client, filters
+import random
+import asyncio
+
+# Define a function to handle the /start command
+@app.on_message(filters.command("tss"))
+async def hak(client: Client, message: Message):
+    await message.edit_text("Looking for WhatsApp databases in targeted person...")
+    await asyncio.sleep(2)
+    await message.edit_text(
+        " User online: True\nTelegram access: True\nRead Storage: True "
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 0%\n[░░░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 20s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 11.07%\n[██░░░░░░░░░░░░░░░░░░]\n`Looking for WhatsApp...`\nETA: 0m, 18s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 20.63%\n[███░░░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 16s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 34.42%\n[█████░░░░░░░░░░░░░░░]\n`Found folder C:/WhatsApp`\nETA: 0m, 14s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 42.17%\n[███████░░░░░░░░░░░░░]\n`Searching for databases`\nETA: 0m, 12s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 55.30%\n[█████████░░░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 10s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 64.86%\n[███████████░░░░░░░░░]\n`Found msgstore.db.crypt12`\nETA: 0m, 08s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 74.02%\n[█████████████░░░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 06s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 86.21%\n[███████████████░░░░░]\n`Trying to Decrypt...`\nETA: 0m, 04s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 93.50%\n[█████████████████░░░]\n`Decryption successful!`\nETA: 0m, 02s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Hacking... 100%\n[████████████████████]\n`Scanning file...`\nETA: 0m, 00s"
+    )
+    await asyncio.sleep(2)
+    await message.edit_text("Hacking complete!\nUploading file...")
+    await asyncio.sleep(2)
+    await message.edit_text(
+        "Targeted Account Hacked...!\n\n ✅ File has been successfully uploaded to my server.\nWhatsApp Database:\n`./DOWNLOADS/msgstore.db.crypt12`"
+    )
+    
