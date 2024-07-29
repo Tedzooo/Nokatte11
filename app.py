82d076707c8ed2e67242e826ebc76066439d0069
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-29 13:04:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-if __name__ == '__master__':
+if __name__ == '__main__':
     # Run the Flask app on port 80
     app.run(host='0.0.0.0', port=80)
