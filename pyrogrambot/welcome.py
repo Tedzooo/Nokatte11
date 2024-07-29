b11f0713213fec4da216cd275d5ec34048b535e0
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-29 10:38:35 +0530

    Update welcome.py

--- pyrogrambot/welcome.py
+++ pyrogrambot/welcome.py
@@ -1,5 +1,4 @@
-from pyrogram import Client, Emoji, Filters
-
+from pyrogram import Client, filters
 MENTION = "[{}](tg://user?id={})"
 MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.ml/)'s group chat {}!"
 
@@ -9,5 +8,5 @@ chats_filter = Filters.chat(["PyrogramChat", "PyrogramLounge"])
 @Client.on_message(chats_filter & Filters.new_chat_members)
 def welcome(client, message):
     new_members = [MENTION.format(i.first_name, i.id) for i in message.new_chat_members]
-    text = MESSAGE.format(Emoji.SPARKLES, ", ".join(new_members))
+    text = MESSAGE.format(hyy, ", ".join(new_members))
     message.reply(text, disable_web_page_preview=True)
