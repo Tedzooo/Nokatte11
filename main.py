e1f5181791755d8ad4065e2af3ebe6cb56eb9d86
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-29 11:54:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from pyrogram import Client, filters
 import asyncio
 import os
 from dotenv import load_dotenv
-API_ID = '15453419'
-API_HASH = '6c9c9e5a2e65daf192e7dd9dde026f45'
-BOT_TOKEN = '7161717671:AAF4VBmLu3JGECaPXMdVPxrawt3SrE0LbcQ'
-# Define a list of prank messages
+from vars import *
 
-# Initialize the Clien
-group_id = '-1002006186805'
+
+# Initialize the Client
 
 
 load_dotenv()
@@ -22,25 +19,5 @@ app= Client(
     plugins=dict(root="pyrogrambot")
 )
 
-@app.on_message(filters.chat(group_id) & ~filters.me)
-async def process_message(client, message):
-    try:
-        text = message.text.lower()
-        if text in ['hi', 'hello']:
-            sent_message = await message.reply("Hello there, welcome to community.")
-            print(f"Message sent successfully: {sent_message.text}")
-    except Exception as e:
-        print(f"Failed to send message: {e}")
 
-async def main():
-    try:
-        await app.start()
-        print("Bot started")
-        # Keep the bot running
-        while True:
-            await asyncio.sleep(10)
-    except Exception as e:
-        print(f"Error starting bot: {e}")
-    finally:
-        await app.stop()
 app.run()
