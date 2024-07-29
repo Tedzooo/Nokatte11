685cb970c4d086af1ea4c4fb7d02818294a43fcc
TEDZOBOT <94851911+Tedzo555@users.noreply.github.com>
2024-07-29 11:06:09 +0530

    Update welcome.py

--- pyrogrambot/welcome.py
+++ pyrogrambot/welcome.py
@@ -1,11 +1,11 @@
-from pyrogram import Client, Filters
+from pyrogram import Client, filters
 MENTION = "[{}](tg://user?id={})"
 MESSAGE = "{} Welcome to [Pyrogram](https://docs.pyrogram.ml/)'s group chat {}!"
 
-chats_filter = Filters.chat(["PyrogramChat", "PyrogramLounge"])
+chats_filter = filters.chat(["PyrogramChat", "PyrogramLounge"])
 
 
-@Client.on_message(chats_filter & Filters.new_chat_members)
+@Client.on_message(chats_filter & filters.new_chat_members)
 def welcome(client, message):
     new_members = [MENTION.format(i.first_name, i.id) for i in message.new_chat_members]
     text = MESSAGE.format(hyy, ", ".join(new_members))
